059f61b37f6a9d51d75d9bb654e24e50c4a88d51
Omar Mohamed <hssnomr2022@gmail.com>
2025-09-10 23:34:23 +0300

    Adding Drawings

--- mechs/ascii_art.py
+++ mechs/ascii_art.py
@@ -6,3 +6,61 @@ def print_title_screen():
     banner = pyfiglet.figlet_format("The One Hundred Years' War", font="roman", width=columns)
     print(banner.center(columns))
 
+
+longsword = """            //
+()========>>=========================--
+            \\\\"""
+
+battle_axe=r"""  ,  /\  .  
+ //`-||-'\\ 
+(| -=||=- |)
+ \\,-||-.// 
+  `  ||  '  
+     ||     
+     ||     
+     ||     
+     ||     
+     ||     
+     ()"""
+
+# For the lack of ascii art compund_bow and short_bow are going to have the same image
+
+short_bow=r"""   (
+    \
+     )
+##-------->     
+     )
+    /
+   ("""
+
+warhammer=r"""             +-+
+=============| |
+            `:_;'"""
+dagger=r""")xxxxx[;;;;;;;;;>"""
+
+rapier=r"""       |
+       /~\
+Oxxxxx|  (|=========================-
+ \____/\_/
+       |"""
+
+halbred=r"""  ,  /\
+ //`-||
+(| -=||
+ \\,-||
+  `  ||  
+     ||     
+     ||     
+     ||     
+     ||     
+     ||     
+     ()"""
+
+crossbow=r"""                .-.                        
+               /  \\   
+          .---/-+--||   
+          |  K=====++-> 
+          '---\-+--||  
+               \  //  
+                `-' """
+print(crossbow)
