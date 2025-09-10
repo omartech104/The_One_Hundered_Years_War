c5aeb0262501fe744780fde494881d97e69a6999
Omar Mohamed <hssnomr2022@gmail.com>
2025-09-10 22:41:25 +0300

    Fixing the ASCII Art

--- mechs/ascii_art.py
+++ mechs/ascii_art.py
@@ -1,11 +1,8 @@
+import shutil
 import pyfiglet
 
 def print_title_screen():
-    # Adjust width so the roman font stays readable
-    banner = pyfiglet.figlet_format(
-        "The One Hundred Years' War",
-        font="roman",
-        width=120  # try 80, 100, 120 depending on your terminal
-    )
-    print(banner)
+    columns = shutil.get_terminal_size().columns
+    banner = pyfiglet.figlet_format("The One Hundred Years' War", font="roman", width=columns)
+    print(banner.center(columns))
 
